ec4be0726f2cc844147a038c98015b22c40288ba
Laura Portillo <lauraportillo.ark@gmail.com>
2021-03-03 16:42:31 +0100

    start the exercise

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Filters from './Filters';
+import CharacterList from './CharacterList';
+import CharacterDetail from './CharacterDetail';
+import getDataFromApi from '../services/getDataFromApi';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
