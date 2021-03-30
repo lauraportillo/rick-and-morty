1bbc8c284f06bb569472fba89340ffe239af4ecb
Laura Portillo <lauraportillo.ark@gmail.com>
2021-03-30 22:32:36 +0200

    add link to my linkedin profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import '../stylesheets/Footer.scss';
 const Footer = () => {
   return (
     <footer className="containerFooter">
-      <small>By Laura Portillo Rodríguez</small>
+      <a href="https://www.linkedin.com/in/laura-portillo-rodríguez" target="_blank" className="containerFooter__name">
+        <small>By Laura Portillo Rodríguez</small>
+      </a>
       <small>©2021</small>
     </footer>
   );
